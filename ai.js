32650c45b32da1e2f1629c3eb1c0ea83eab0649a
mpa-LHutchinson <49694715+mpa-LHutchinson@users.noreply.github.com>
2024-09-25 12:11:22 -0400

    added functionality to specify multiple AI models

--- ai.js
+++ ai.js
@@ -1,16 +1,16 @@
 require('dotenv').config({ path: './.env' });
 const Groq = require("groq-sdk");
-const model = "llama3-8b-8192"
+const models = ["llama3-8b-8192", "mixtral-8x7b-32768", "llava-v1.5-7b-4096-preview"];
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-async function createREADME(fileContents, fileNames) {
-  const chatCompletion = await getGroqChatCompletion(fileContents, fileNames);
+async function createREADME(fileContents, fileNames, modelChange) {
+  const chatCompletion = await getGroqChatCompletion(fileContents, fileNames, modelChange);
   return (chatCompletion.choices[0]?.message?.content || "");
 }
 
 // Function to get Groq Chat Completion and token usage
-async function getGroqChatCompletion(fileContents, fileNames) {
+async function getGroqChatCompletion(fileContents, fileNames, modelChange) {
   const response = await groq.chat.completions.create({
     messages: [
       {
@@ -18,15 +18,15 @@ async function getGroqChatCompletion(fileContents, fileNames) {
         content: "My files have this content in it: " + fileContents + "The name of the files are " + fileNames + " Generate a README file for a project that uses this code.",
       },
     ],
-    model: model,
+    model: models[modelChange],
   });
 
   return response;
 }
 
 // Function to retrieve token usage from the Groq API
-async function getTokenUsage(fileContents, fileNames) {
-  const response = await getGroqChatCompletion(fileContents, fileNames);
+async function getTokenUsage(fileContents, fileNames, modelChange) {
+  const response = await getGroqChatCompletion(fileContents, fileNames, modelChange);
 
   return {
     prompt: response.usage?.prompt_tokens || 0,        // Check for prompt tokens in the API response
@@ -34,4 +34,4 @@ async function getTokenUsage(fileContents, fileNames) {
   };
 }
 
-module.exports = { createREADME, getTokenUsage, model };
+module.exports = { createREADME, getTokenUsage, models };
